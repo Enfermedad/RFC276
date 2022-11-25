9d61a1d804095e77c74f49c9b207b0c1668e4e6f
Enfermedad <tomygmbb@gmail.com>
2022-11-25 16:42:12 -0300

    NTP time Fix

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import time
 import socket
 import struct
+import sys
 localIP     = "127.0.0.1"
 localPort   = 123
 NTPFORMAT = "!B B B b 11I"
@@ -25,10 +26,19 @@ def system_to_ntp(t=0.0):
     # Transform system timestamp to NTP timestamp
     #------#
     t+= NTPDELTA
-    return (int(t) <<32) + (int(abs(t-int(t)*(1<<32))))
+    return (t)
 
 def to_time(integ, frac, n=32):
     return integ + float(frac)/2**n	
+
+def _to_int(timestamp):
+    """Return the integral part of a timestamp.
+    Parameters:
+    timestamp -- NTP timestamp
+    Retuns:
+    integral part
+    """
+    return int(timestamp)
 
 def to_frac(timestamp, n=32):
     return int(abs(timestamp - _to_int(timestamp)) * 2**n)
@@ -68,14 +78,29 @@ while(True):
     precision = acc
     root_delay = 0
     root_dispersion = 0     #calcular segun shoa
-    ref_id = 0xc81b6a73     #200.27.106.115 to hex
+    ref_id =0xc81b6a73     #200.27.106.115 to hex
     ref_timestamp= serverrecv
     recv_timestamp = serverrecv
     tx_timestamp = system_to_ntp(time.time())
+
     
     # Enviar respuesta
-    data = struct.pack(NTPFORMAT, leap << 6 | version << 3 | mode, stratum, poll, precision, int(root_delay) << 16 | to_frac(root_delay,16),
-     int(root_dispersion) << 16 | to_frac(root_dispersion,16), ref_id, int(ref_timestamp), to_frac(ref_timestamp), int(orig_timestamp), to_frac(orig_timestamp),
-     int(recv_timestamp), to_frac(recv_timestamp), int(tx_timestamp), to_frac(tx_timestamp))
-    
+    #data = struct.pack(NTPFORMAT, leap << 6 | version << 3 | mode, stratum, poll, precision, int(root_delay) << 16 | to_frac(root_delay,16),int(root_dispersion) << 16 | to_frac(root_dispersion,16), ref_id, int(ref_timestamp), to_frac(ref_timestamp), int(orig_timestamp), to_frac(orig_timestamp),int(recv_timestamp), to_frac(recv_timestamp), int(tx_timestamp), to_frac(tx_timestamp))
+    data = struct.pack(NTPFORMAT,
+     (leap << 6 | version << 3 | mode)
+     , stratum
+     , poll
+     , precision
+     , int(root_delay) << 16 | to_frac(root_delay,16)
+     ,int(root_dispersion) << 16 | to_frac(root_dispersion,16)
+     ,ref_id
+     ,int(ref_timestamp)#int(ref_timestamp)
+     ,to_frac(ref_timestamp)
+     ,int(orig_timestamp)
+     ,to_frac(orig_timestamp)
+     ,int(recv_timestamp)
+     ,to_frac(recv_timestamp)
+     ,int(tx_timestamp)
+     ,to_frac(tx_timestamp)
+     )
     UDPServerSocket.sendto(data, addr)
